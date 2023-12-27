9bd634c3d92023a034d97b1ccd9977efc3169b08
agent LuluDodo <luludodo.gaming@gmail.com>
2023-12-28 00:08:58 +0100

    Added betterf3 support, yay

---
+++ src/main/java/de/luludodo/dmc/mixins/betterf3/ChunksModuleMixin.java
@@ -0,0 +1,20 @@
+package de.luludodo.dmc.mixins.betterf3;
+
+import de.luludodo.dmc.api.DMCApi;
+import me.cominixo.betterf3.modules.ChunksModule;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(ChunksModule.class)
+public class ChunksModuleMixin {
+    @ModifyArg(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/ChunkPos;<init>(Lnet/minecraft/util/math/BlockPos;)V", ordinal = 0), index = 0)
+    public BlockPos getBlockPos(BlockPos original) {
+        return new BlockPos(
+                (int) DMCApi.getOffsetBlockX(original.getX()),
+                (int) DMCApi.getOffsetBlockY(original.getY()),
+                (int) DMCApi.getOffsetBlockZ(original.getZ())
+        );
+    }
+}
